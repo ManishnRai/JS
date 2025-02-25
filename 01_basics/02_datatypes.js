3c9ace9435bf65255e00de38ec3a7e6cc88fbf53
ManishnRai <125142120+ManishnRai@users.noreply.github.com>
2025-02-25 17:38:00 +0000

    understood data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";
+// treat all the code as  new JS version.
+// alert(3+3)// we are uing node.js not browser
+console.log(3+3); console.log("Hitesh")
+let name ="manish"
+let age = 27
+let isLoggedIn =true
+
+//number=>2 to the power 53
+// bigint =>
+// string => ""
+// boolean => true/false
+// null => stand alone value
+//undefined => 
+// symbol=> unique
+
+// object
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
