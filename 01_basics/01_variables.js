25b804a6ce5648680d2b510617671f571a83b497
ManishnRai <125142120+ManishnRai@users.noreply.github.com>
2025-02-25 09:47:22 +0000

    Learning about defining a variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accId = 12345
+let accountEmail="manish@gmail.com"
+var accountPassword ="123456"
+accountCity = "New Delhi"
+
+// to print all the dataina tabular format use console.table.
+console.table([accId,accountEmail,accountPassword,accountCity])
+/*to define a variable we use let and to define a constant we use const, var is aslo used but due to its scope issue its 
+not frequently used.*/
