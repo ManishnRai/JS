a2f1998f37225fb1cde2a5b127930778a063aafd
ManishnRai <125142120+ManishnRai@users.noreply.github.com>
2025-02-25 17:55:10 +0000

    learn conversion

---
+++ 01_basics/03_conversionOperaton.js
@@ -0,0 +1,15 @@
+let score = undefined;
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);//NaN
+// "33" => 33
+//"33abc"=> NaN(not a number) type is number
+// true=> 1, false=> 0
+//undefined=> NaN type is number
+
+// convert to string
+// 1=> true,0=> false
+//"" => false
+//"Manish => true"
